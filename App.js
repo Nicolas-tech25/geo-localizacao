940dae5cd1d4611afddb91cf6261690f7c37e124
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-03-25 09:27:37 -0300

    aaa

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import { StyleSheet, Text, View, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   const regiaoInicialMapa = {
-    latitude: -10,
-    longitude: -55,
+    // latitude: -10,
+    // longitude: -55,
+
+    latitude: -23.533773,
+    longitude: -46.65529,
     latitudeDelta: 40,
     longitudeDelta: 40,
+  };
+  const localizacao = {
+    latitude: -33.867886,
+    longitude: -63.987,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
   };
   return (
     <>
       <StatusBar style="auto" />
       <View style={estilo.container}>
         <MapView
-          mapType="hybrid"
+          mapType="standart"
           style={estilo.mapa}
           initialRegion={regiaoInicialMapa}
           userInterfaceStyle="dark "
-          maxZoomLevel={15}
-          minZoomLevel={5}
-        />
+          // maxZoomLevel={15}
+          // minZoomLevel={5}
+        >
+          <Marker coordinate={localizacao}>
+            <Image source={require("./assets/ghost.png")} />
+          </Marker>
+        </MapView>
       </View>
     </>
   );
